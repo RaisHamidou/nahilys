b992e3bf9b57f44a0224b202cd823547cb3fafbf
Numidian-Dev <numidiandev@gmail.com>
2026-02-25 19:16:40 +0100

    url

--- components/Config/Config.js
+++ components/Config/Config.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
 
 const dev = process.env.NODE_ENV != "production";
-export const URL = dev ? "http://localhost:4050" : "https://nahilys-git-main-raishamidou1s-projects.vercel.app";
+export const URL = dev ? "http://localhost:4050" : "https://nahilys.vercel.app";
 
